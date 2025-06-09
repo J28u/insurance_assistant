d340cfff7c71fa0be5a66bd76ae3d608c48d4840
J28u <judithcecchin@gmail.com>
2025-06-09 21:15:54 +0200

    :building_construction: Initialize kedro package structure for rag pipelines

---
+++ kedro_pipelines/src/rag/pipeline_registry.py
@@ -0,0 +1,16 @@
+"""Project pipelines."""
+
+from kedro.pipeline import Pipeline
+from kedro.framework.project import find_pipelines
+
+
+def register_pipelines() -> dict[str, Pipeline]:
+    """Register the project's pipelines.
+
+    Returns:
+        A mapping from pipeline names to ``Pipeline`` objects.
+    """
+
+    pipelines = find_pipelines()
+    pipelines["__default__"] = sum(pipelines.values())
+    return pipelines
