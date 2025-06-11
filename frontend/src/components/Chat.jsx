81670171807a879342e3f216e9a07486eb534d12
J28u <judithcecchin@gmail.com>
2025-06-12 00:40:51 +0200

    :sparkles: Add Chat component for conversation page display

---
+++ frontend/src/components/Chat.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import "../style.css";
+import DeepseekInput from "./DeepseekInput.jsx";
+import Conversation from "./Conversation.jsx";
+
+function Chat({
+  setNewConversation,
+  newConversation,
+  newChat,
+  setConversationTitle,
+  conversationTitle,
+  conversationId,
+  setConversationId,
+  response,
+  setResponse,
+  setLoading,
+  loading,
+  convLoading,
+  setPrompt,
+  prompt,
+  messages,
+  setMessages,
+  showFirstMessages,
+  setShowFirstMessages,
+}) {
+  return (
+    <div>
+      <div>
+        <Conversation
+          conversationTitle={conversationTitle}
+          convLoading={convLoading}
+          messages={messages}
+          loading={loading}
+          conversationId={conversationId}
+        />
+      </div>
+      <div>
+        <DeepseekInput
+          setNewConversation={setNewConversation}
+          newConversation={newConversation}
+          newChat={newChat}
+          setConversationTitle={setConversationTitle}
+          conversationId={conversationId}
+          setConversationId={setConversationId}
+          response={response}
+          setResponse={setResponse}
+          setLoading={setLoading}
+          loading={loading}
+          setPrompt={setPrompt}
+          prompt={prompt}
+          messages={messages}
+          setMessages={setMessages}
+          showFirstMessages={showFirstMessages}
+          setShowFirstMessages={setShowFirstMessages}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
