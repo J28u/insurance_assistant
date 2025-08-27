188497609bd104105e6d1d7be49c666ac548dd49
J28u <judithcecchin@gmail.com>
2025-08-27 18:52:14 +0200

    :memo: Add JSDoc comments to verifyFirebaseToke middleware

--- backend/src/middlewares/verifyFirebaseToken.js
+++ backend/src/middlewares/verifyFirebaseToken.js
@@ -1,8 +1,19 @@
-// Middleware de vérification du token
-
-// backend/src/middlewares/verifyFirebaseToken.js
 const admin = require("../firebaseAdmin");
 
+/**
+ * Middleware Express pour vérifier un token Firebase ID.
+ *
+ * <ul>
+ * <li> - Récupère le token depuis l'en-tête Authorization (format: "Bearer <token>").</li>
+ * <li> - Vérifie le token via Firebase Admin SDK (signature, expiration, qu'il vient du projet Firebase)</li>
+ * <li> - Ajoute l'utilisateur décodé (UID, email ...) dans `req.firebaseUser` si le token est valide, pour l'utiliser dans les routes.</li>
+ * </ul>
+ *
+ * @param {object} req - Requête Express entrante
+ * @param {object} res - Réponse Express
+ * @param {Function} next - Fonction pour passer au middleware suivant
+ * @returns {Promise<void>}
+ */
 const verifyFirebaseToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
@@ -10,11 +21,11 @@ const verifyFirebaseToken = async (req, res, next) => {
     return res.status(401).json({ message: "Token manquant ou invalide" });
   }
 
-  const idToken = authHeader.split(" ")[1];
+  const idToken = authHeader.split(" ")[1]; // On extrait le token Firebase ID : partie après "Bearer "
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken);
-    req.firebaseUser = decodedToken; // stocke tout le token décodé
+    req.firebaseUser = decodedToken;
     next();
   } catch (error) {
     console.error("Erreur de vérification Firebase:", error);
